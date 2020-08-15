fd14814b4e14e8fb590a1f3f12115c3dc7fbe6ee
Christian Kischy Haege <kischy@gmx.de>
2020-08-15 13:25:52 +0200

    Added Date class and empty test file

---
+++ p19-euler/p19-euler-test/test-Date.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+
+#include "../p19-euler/Date.h"
+
+
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
---
+++ p19-euler/p19-euler/p19-euler.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Calculation started" << endl;
+
+
+    int answer_p19 = 0;
+
+
+
+    cout << "The answer to problem 19 of project Euler is " << answer_p19 << "." << endl;
+}
+

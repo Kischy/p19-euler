6f4f4be716b099ce4c0f1f3994cd020f7ba6db0c
Christian Kischy Haege <kischy@gmx.de>
2020-08-15 13:54:07 +0200

    Added isBigger operator for Date class

--- p19-euler/p19-euler-test/test-Date.cpp
+++ p19-euler/p19-euler-test/test-Date.cpp
@@ -3,8 +3,64 @@
 #include "../p19-euler/Date.h"
 
 
+class DateOperatorDayTests : public testing::Test
+{
+public:
+	Date biggerDate = Date(02, 01, 1900);
+	Date smallerDate = Date(01, 01, 1900);
+};
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+
+TEST_F(DateOperatorDayTests, IsBigger)
+{
+	EXPECT_TRUE(biggerDate > smallerDate);
+	EXPECT_FALSE(smallerDate > biggerDate);
 }
+
+TEST_F(DateOperatorDayTests, IsSmaller)
+{
+	EXPECT_FALSE(biggerDate < smallerDate);
+	EXPECT_TRUE(smallerDate < biggerDate);
+}
+
+class DateOperatorMonthTests : public testing::Test
+{
+public:
+	Date biggerDate = Date(01, 02, 1900);
+	Date smallerDate = Date(01, 01, 1900);
+};
+
+
+TEST_F(DateOperatorMonthTests, IsBigger)
+{
+	EXPECT_TRUE(biggerDate > smallerDate);
+	EXPECT_FALSE(smallerDate > biggerDate);
+}
+
+TEST_F(DateOperatorMonthTests, IsSmaller)
+{
+	EXPECT_FALSE(biggerDate < smallerDate);
+	EXPECT_TRUE(smallerDate < biggerDate);
+}
+
+
+class DateOperatorYearTests : public testing::Test
+{
+public:
+	Date biggerDate = Date(01, 01, 1901);
+	Date smallerDate = Date(01, 01, 1900);
+};
+
+
+TEST_F(DateOperatorYearTests, IsBigger)
+{
+	EXPECT_TRUE(biggerDate > smallerDate);
+	EXPECT_FALSE(smallerDate > biggerDate);
+}
+
+TEST_F(DateOperatorYearTests, IsSmaller)
+{
+	EXPECT_FALSE(biggerDate < smallerDate);
+	EXPECT_TRUE(smallerDate < biggerDate);
+}
+
--- p19-euler/p19-euler/Date.h
+++ p19-euler/p19-euler/Date.h
@@ -1,7 +1,64 @@
 #pragma once
+
 class Date
 {
 public:
+	Date(int day, int month, int year)
+	{
+		this->day = day;
+		this->month = month;
+		this->year = year;
+	}
+
+
+	bool operator>(const Date& rhs)
+	{
+		if (this->year > rhs.year)
+		{
+			return true;
+		}
+
+		if (this->year == rhs.year)
+		{
+			if (isBiggerMonth(rhs.month, rhs.day))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+
+	bool operator<(const Date& rhs)
+	{
+		return true;
+	}
+
+
+private:
+	int day, month, year;
+
+
+	bool isBiggerMonth(const int rhs_month, const int rhs_day)
+	{
+		if (this->month > rhs_month)
+		{
+			return true;
+		}
+
+		if (this->month == rhs_month)
+		{
+			if (this->day > rhs_day)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+
 
 
 };
